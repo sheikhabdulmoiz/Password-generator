5fd40a42ba3d5e8093cf237d2a62a40e7a010c67
Abdul Moiz <abdulmoiz15000@gmail.com>
2022-06-30 20:31:31 +0500

    p.g by random number

---
+++ app.js
@@ -0,0 +1,12 @@
+var A="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrsruvwxyz0123456789!@#$%^&*?";
+
+var q=Math.floor(Math.random()*A.length);
+console.log(q);
+var passcode="";
+
+for(var i=0;i<8;i++){
+    var q=Math.floor(Math.random()*A.length);
+    passcode+=A[q];
+}
+    document.write("<center>"+"New Passcode: " + "<b>"+passcode+"</b></center>");
+
